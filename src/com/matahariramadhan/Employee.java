91a0dde547da1c119a77b2787b92cf13d036582e
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-04-26 01:01:17 +0200

    abstracting setter and adding constructor

--- src/com/matahariramadhan/Employee.java
+++ src/com/matahariramadhan/Employee.java
@@ -4,11 +4,16 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
+    public Employee(int baseSalary, int hourlyRate){
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
+
     public int calculateWage(int extraHours){
         return baseSalary + (extraHours * hourlyRate);
     }
 
-    public void setBaseSalary(int baseSalary){
+    private void setBaseSalary(int baseSalary){
         if (baseSalary <= 0)
             throw  new IllegalArgumentException("Base Salary cannot less then or equal to 0");
 
@@ -19,7 +24,7 @@ public class Employee {
         return baseSalary;
     }
 
-    public void setHourlyRate(int hourlyRate){
+    private void setHourlyRate(int hourlyRate){
         if (hourlyRate <= 0)
             throw new IllegalArgumentException("Hourly Rate cannot less then or equal to 0");
 

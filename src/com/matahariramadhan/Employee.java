23591c436a841680512e4e6e4717dbff9ed3df31
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-04-26 00:54:49 +0200

    adding setter and getter

--- src/com/matahariramadhan/Employee.java
+++ src/com/matahariramadhan/Employee.java
@@ -1,10 +1,32 @@
 package com.matahariramadhan;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     public int calculateWage(int extraHours){
         return baseSalary + (extraHours * hourlyRate);
+    }
+
+    public void setBaseSalary(int baseSalary){
+        if (baseSalary <= 0)
+            throw  new IllegalArgumentException("Base Salary cannot less then or equal to 0");
+
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary(){
+        return baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate){
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly Rate cannot less then or equal to 0");
+
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
     }
 }

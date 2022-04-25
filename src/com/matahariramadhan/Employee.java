7d97a641f0ed0cc7a36d778fad85a1ede9eed3bc
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-04-25 21:55:32 +0200

    implementing encapsulation

---
+++ src/com/matahariramadhan/Employee.java
@@ -0,0 +1,10 @@
+package com.matahariramadhan;
+
+public class Employee {
+    public int baseSalary;
+    public int hourlyRate;
+
+    public int calculateWage(int extraHours){
+        return baseSalary + (extraHours * hourlyRate);
+    }
+}
--- src/com/matahariramadhan/Main.java
+++ src/com/matahariramadhan/Main.java
@@ -1,15 +1,13 @@
 package com.matahariramadhan;
+import com.matahariramadhan.Employee;
 
 public class Main {
     public static void main(String[] args) {
-        int baseSalary = 50_000;
-        int extraHours = 10;
-        int hourlyRate = 20;
+        var employee = new Employee();
+        employee.baseSalary = 50_000;
+        employee.hourlyRate = 20;
+        int wage = employee.calculateWage(10);
 
-        System.out.println(calculateWage(baseSalary,extraHours,hourlyRate));
-    }
-    public static int calculateWage(int baseSalary, int extraHours, int hourlyRate){
-        int wage = baseSalary + (extraHours * hourlyRate);
-        return wage;
+        System.out.println(wage);
     }
 }
